312fc829401efe33a844b5e62e7c4a7fcfbb9c9a
doctorzalman <georgesun30@gmail.com>
2020-11-20 10:09:49 +0200

    pre_compres

---
+++ category.php
@@ -0,0 +1,46 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+get_header();
+//Single post blog
+the_post();
+?>
+<?php get_template_part('template-parts/title/title'); ?>
+
+  <!--blog list-->
+  <div class="blogpage pagepadding ">
+    <div class="container">
+      <div class="row blog content-sidebar">
+
+          <div class="col-sm-12 col-md-8 content-area">
+          <div class="blog-details">
+            <div class="entry-header">
+              <div class="entry-thumbnail margbtm30">
+                <?php the_post_thumbnail( 'single_post_page', 'photo' ); ?>
+              </div>
+              <h1 class="entry-title">
+                <a href="#"><?php the_title(); ?></a>
+              </h1>
+              <div class="entry-meta">
+                <a href="#"><?php echo get_the_date('F d Y') ?></a>
+              </div>
+            </div>
+            <div class="entry-content">
+             <?php the_content(); ?>
+              <br>
+              <div class="entry-footer">
+                <span class="tags-links">
+                  <?php the_tags('Теги: ');?>
+                </span>
+<!--	              --><?php //get_template_part('template-parts/buttons/social' ); ?>
+              </div>
+	            <?php get_template_part('template-parts/buttons/navigation' ); ?>
+	            <?php comments_template(); ?>
+            </div>
+          </div>
+        </div>
+	      <?php get_template_part('template-parts/sidebar/right_sidebar'); ?>
+      </div>
+    </div>
+  </div>
+  <!--blog list end-->
+<?php get_footer();?>
---
+++ search.php
@@ -0,0 +1,7 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+get_header();
+//404 page
+?>
+404 page
+<?php get_footer();?>
